523b29684b1cdf8757a6df894fcfe63b5d292a74
guilhermevictor <guivictor2011@gmail.com>
2015-11-17 20:39:52 -0200

    Bet dialog done

--- app/src/main/java/mc750/cronos/apostae/bet/BetChooseActivity.java
+++ app/src/main/java/mc750/cronos/apostae/bet/BetChooseActivity.java
@@ -1,6 +1,7 @@
 package mc750.cronos.apostae.bet;
 
 import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -11,9 +12,11 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 
 
 import mc750.cronos.apostae.R;
+import mc750.cronos.apostae.library.Utils;
 
 public class BetChooseActivity extends AppCompatActivity {
 
@@ -62,35 +65,54 @@ public class BetChooseActivity extends AppCompatActivity {
             super.onBackPressed();
         }
     }
-    private void makeDialog(Context context) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(context);
-        builder.setTitle("Test");
-        builder.setIcon(R.drawable.apostae);
-        builder.setMessage("test");
-        builder.setPositiveButton("Call Now",
-                new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        dialog.cancel();
+    private void makeDialog(final Context context) {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(context);
 
+        //builder.setTitle(getString(R.string.bet_dialogTitle));
+
+        final View dialogView = getLayoutInflater().inflate(R.layout.dialog_bet, null);
+        builder.setView(dialogView);
+
+        builder.setPositiveButton(getString(R.string.bet_positive), null);
+        //builder.setNeutralButton(getString(R.string.bet_neutral), null);
+        builder.setNegativeButton(getString(R.string.bet_negative), null);
+
+        final AlertDialog dialog = builder.create();
+        dialog.setOnShowListener(new DialogInterface.OnShowListener() {
+            @Override
+            public void onShow(DialogInterface arg0) {
+                Button positive = dialog.getButton(AlertDialog.BUTTON_POSITIVE);
+                //Button neutral = dialog.getButton(AlertDialog.BUTTON_NEUTRAL);
+                Button negative = dialog.getButton(AlertDialog.BUTTON_NEGATIVE);
+
+                positive.setTextColor(getResources().getColor(R.color.colorPrimaryDark));
+                //neutral.setTextColor(getResources().getColor(R.color.colorPrimary));
+                negative.setTextColor(getResources().getColor(R.color.colorPrimaryDark));
+
+
+                positive.setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View v) {
+                        Utils.snackBar(dialogView, getString(R.string.not_implemented));
                     }
                 });
 
-        builder.setNeutralButton("Setup",
-                new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-
-
+                negative.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog.dismiss();
                     }
                 });
 
-        builder.setNegativeButton("Exit",
-                new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        dialog.cancel();
-
+                /*neutral.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Utils.snackBar(dialogView, getString(R.string.not_implemented));
                     }
-                });
+                });*/
+            }
+        });
 
-        builder.show();
+
+        dialog.show();
     }
 }
